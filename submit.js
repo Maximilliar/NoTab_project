c3c6ed90a427e24adc9de1f2100a3ca0a9661469
Maximilliar <maximmakar@gmail.com>
2021-09-27 21:53:02 +0600

    Add new functions in JS

--- submit.js
+++ submit.js
@@ -1,18 +1,58 @@
 const submitButton = document.querySelector('#send');
-const form = document.querySelectorAll('.form_input, .form_input50');
+const forms = document.querySelectorAll('.form_input, .form_input50');
+const divApps = document.querySelector('.download_apps_ios_android');
 
-submitButton.addEventListener('click', function () {
-    form.forEach((val) => {
-        if (val.value) {
-            console.log(val.value);
-            if (val.classList.contains('red-border')) {
-                val.classList.remove('red-border');
-                
-            }
-        } else {
-            val.classList.add('red-border');
+const formName = document.getElementsByName('firstName')[0],
+    formVenueName = document.getElementsByName('venueName')[0],
+    formEmail = document.getElementsByName('email')[0],
+    formSubject = document.getElementsByName('subject')[0],
+    formVenueCity = document.getElementsByName('venueCity')[0],
+    formStateCity = document.getElementsByName('stateCity')[0],
+    formMessage = document.getElementsByName('message')[0];
+
+submitButton.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    forms.forEach((item) => {
+        if (item.value == '') {
+            item.classList.add('red-border');
+            submitButton.disabled = true;
+            setTimeout(() => submitButton.disabled = false, 5000);
         }
-        val.value = "";
+        if (item.value) {
+            if (item.classList.contains('red-border')) {
+                item.classList.remove('red-border');
+            }
+        }
     });
+
+    checkRedBorder();
 });
 
+function checkRedBorder() {
+    if ((formName || formVenueName || formEmail || formSubject || formMessage || formVenueCity || formStateCity).classList.contains('red-border')) {
+        showMessage('Произошла ошибка! Заполните все поля ввода.');
+    } else {
+        formName.value = '';
+        formVenueName.value = '';
+        formEmail.value = '';
+        formSubject.value = '';
+        formMessage.value = '';
+        formStateCity.value = '';
+        formVenueCity.value = '';
+        showMessage('Спасибо за обращение! Мы с вами свяжемся :)');
+    }
+}
+
+function showMessage(message) {
+    const messageBlock = document.createElement('div');
+    messageBlock.innerHTML = `
+            <h1>${message}</h1>
+            `;
+    messageBlock.style.cssText = `
+            text-align: center;
+            margin-top: -82px;
+            `;
+    divApps.prepend(messageBlock);
+    setTimeout(() => messageBlock.remove(), 5000);
+}
